1131c556ccc6cd3abdaccbe16720a408c835beeb
swapnilpatil0655 <145130883+swapnilpatil0655@users.noreply.github.com>
2026-02-21 12:00:35 +0530

    Standard_library
    Standard library and their supporting files like packages filder and __init__.py files to make package as package

---
+++ Generative AI/Python/Day_5_Importing_Creating_modules_And_Packages/package/__init__.py
@@ -0,0 +1,5 @@
+''' __init__.py is a special file in Python to 
+define packages and initialize their namespaces.''' 
+
+#__init__.py can be empty, but it can also contain code to initialize the package or define what is available when the package is imported.
+# this file make the folder to a package and we can import the modules from this package in other files.
